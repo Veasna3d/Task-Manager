86be0be6134b6e7d425c0f0526ec6936a49250ea
Veasna Pen <Veasna3d@gmail.com>
2024-02-27 22:03:35 +0700

    Form Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import Textbox from "../components/Textbox";
+import Button from "../components/Button";
 
 const Login = () => {
   const user = "";
@@ -45,7 +47,7 @@ const Login = () => {
             onSubmit={handleSubmit(submitHandler)}
             className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
           >
-            <div className="">
+            <div className="text-center">
               <p className="text-blue-600 text-3xl font-bold text-center">
                 Welcome Back !
               </p>
@@ -56,15 +58,35 @@ const Login = () => {
 
             <div className="flex flex-col gap-y-5">
               <Textbox
-              placeholder="email@example.com"
-              type="email"
-              name="email"
-              label="Email Address"
-              classNames="w-full rounded-full"
-              register={register("email", {
-                required: "Email Address is required!"
-              })}
-              errors={errors.email ? errors.email.message : ""}
+                placeholder="email@example.com"
+                type="email"
+                name="email"
+                label="Email Address"
+                className="w-full rounded-full"
+                register={register("email", {
+                  required: "Email Address is required!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder="your password"
+                type="password"
+                name="password"
+                label="Password"
+                className="w-full rounded-full"
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
+              <span className="text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer">
+                Forget Password ?
+              </span>
+
+              <Button
+                type="submit"
+                label="Log In"
+                className="w-full h-10 bg-blue-700 text-white rounded-full"
               />
             </div>
           </form>
